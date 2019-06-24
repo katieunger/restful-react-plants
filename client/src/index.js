77836903a95d520629844f1244271b5ca52cb9a4
Katie Unger <katie.e.unger@gmail.com>
2019-06-24 15:50:23 -0400

    hooking up redux devtools

--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css'
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
     reducers,
-    applyMiddleware(reduxThunk)
+    composeEnhancers(applyMiddleware(reduxThunk))
 );
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.querySelector('#root'));
