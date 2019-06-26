d62095cd4ae67556e35f72f3555d91bf8ddb1172
Katie Unger <katie.e.unger@gmail.com>
2019-06-26 09:37:04 -0400

    adding page title, background image, styling

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Container, Header } from 'semantic-ui-react';
 
+import './App.css';
 import MyTable from './Table';
 import MyMenu from './Menu';
 
 const App = () => {
     return (
-        <Container style={{ marginTop: '3em' }}>
-            <Header as='h1'>My Plants</Header>
-            <MyMenu />
-            <MyTable/>
+        <Container className="fluid background">
+            <Container className="main">
+                <Header as='h1'>My Plants</Header>
+                <MyMenu />
+                <MyTable/>
+            </Container>
         </Container>
     );
 };
